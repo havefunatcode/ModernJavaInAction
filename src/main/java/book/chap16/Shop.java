f7853be39b9bbaa48a742489513a6612e412e4df
havefunatcode <dlxogh439@gmail.com>
2024-07-28 22:22:26 +0900

    Chap 16 학습

---
+++ src/main/java/book/chap16/Shop.java
@@ -0,0 +1,35 @@
+package book.chap16;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Shop {
+    private String name;
+
+    public Shop(String name) {
+        this.name = name;
+    }
+
+    public Future<Double> getPriceAsync(String product) {
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            double price = calculatePrice(product);
+            futurePrice.complete(price);
+        }).start();
+        return futurePrice;
+    }
+
+    private double calculatePrice(String product) {
+        delay();
+        return random.nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+
+    public static void delay() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
